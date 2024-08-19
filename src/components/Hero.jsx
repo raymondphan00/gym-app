0c586ae2ded315999800cd38a8afcf9c6349e3d4
Raymond Phan <raymondphan00@gmail.com>
2024-08-19 17:16:30 -0400

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='min-h-screen flex flex-col gap-10 items-center justify center text-center max-w-[800px] w-full mx-auto p-4'>
+        <div className='flex flex-col gap-4'></div>
+
+        <p>IT'S TIME TO GET</p>
+        <h1 className='uppercase font-semibold text-4xl sm:text-5xl mid:text-6xlxl lg:text-7xl'>Swole<span className='text-blue-400'>normous</span></h1>
+        <p className='text-sm mid:text-base font-light'>I hereby acknowledgment that I may become <span className='text-blue-400 font-medium'>unbelievably swolenormous</span> and accept all risks of becoming the local <span className='text-blue-400 font-medium'>mass montrosisty</span> afflicted with severe body dismorphia, unable to fit through doors.</p>
+        <button className='px-8 py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200'><p>Accept & Begin</p></button>
+    </div>
+  )
+}
